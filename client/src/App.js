860ee8f9a10ed990e05f43772bc01bb3f4ceab07
dprandle <dprandle@alaska.edu>
2021-12-07 05:29:33 -0900

    Before present

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,121 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:3001";
+
+
+function Timeslot_Element(props) {
+    let tab = props?.tab;
+    let val = props?.val;
+    let keyv = props?.keyv;
+    let tabspaces = [];
+    for (let i = 0; i < tab; ++i)
+        tabspaces.push(<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>);
+    if (val !== undefined || typeof (val) === "number")
+        return <div className='TimeslotElement'>{tabspaces}{keyv}: {val}</div>;
+    else
+        return <div className='TimeslotElement'>{tabspaces}{keyv}</div>;
+
+}
+
+function Recurse_Object(source_obj, list, tab_count) {
+    for (const prop in source_obj) {
+        if (typeof (source_obj[prop]) === "object") {
+            list.push(<Timeslot_Element tab={tab_count} key={prop} keyv={prop} />);
+            Recurse_Object(source_obj[prop], list, tab_count + 1);
+        }
+        else {
+            list.push(<Timeslot_Element tab={tab_count} key={prop} keyv={prop} val={source_obj[prop]} />);
+        }
+    }
+}
+
+function Timeslot(props) {
+    let list = [];
+    let tab_count = 0;
+    let ts = props?.ts;
+    //delete ts?.frame_ind;
+    let ins = 'inline-start';
+    if (ts)
+        Recurse_Object(ts, list, tab_count);
+    return (
+        <div className='Timeslot' style={{ float: ins }}>{list}</div>
+    );
+}
+
+function Frame_Title(props) {
+    return <div className='FrameTitle'><h1>Frame: {props?.frame_ind}</h1></div>;
+}
+
+function Frame(props) {
+    let timeslot = props?.timeslots['1'];
+    let frame_data = props?.frame_data;
+    let frame_ind = frame_data?.frame_ind;
+    let timeslots = props?.timeslots;
+    let ts_list = [];
+    ts_list.push(<Timeslot ts={frame_data} />);
+    for (const cur_ts in timeslots) {
+        ts_list.push(<Timeslot ts={timeslots[cur_ts]} />);
+    }
+    return (
+        <div className='Frame'>
+            <Frame_Title frame_ind={frame_ind} />
+            {ts_list}
+        </div>
+    );
+}
+
+function Node_Title(props) {
+    return <div className='NodeTitle'>Node: 0x{props?.src_addr.toString(16)}</div>;
+}
+
+function Node(props) {
+    return <div className="Node">
+        <Node_Title src_addr={props?.src_addr}/>
+        {props?.node}
+    </div>
+}
+
+function Nodes(props) {
+    let nodes = props?.nodes;
+    let timeslots = props?.timeslots;
+
+    let node_list = [];
+    for (const tsind in timeslots) {
+        let src_addr = timeslots[tsind].node_data.src_addr;
+        node_list.push(<Node node={nodes[tsind]} src_addr={src_addr} />);
+    }
+    return (
+        <div className='Nodes'>
+            {node_list}
+        </div>
+    );
+}
 
 function App() {
-    const [prev_msg, set_prev_msg] = React.useState("");
-    const [total_msg, set_total_msg] = React.useState("");
+    const [timeslots, set_timeslots] = React.useState("");
+    const [node_data, set_node_data] = React.useState("");
+    const [frame_data, set_frame_data] = React.useState("");
 
     React.useEffect(() => {
-        setInterval(tick, 200);
-    })
-
-    function tick() {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => {
-                if (data.message !== prev_msg) {
-                    set_prev_msg(data.message);
-                    set_total_msg(total_msg + '\n' + prev_msg);
-                }
-            });
-    }
+        const socket = socketIOClient(ENDPOINT);
+        socket.on("TimeslotData", data => {
+            set_timeslots(data);
+        });
+        socket.on("NodeData", data => {
+            set_node_data(data);
+        });
+        socket.on("FrameData", data => {
+            set_frame_data(data);
+        });
+    }, []);
 
     return (
-        <div className="App"><text>{total_msg}</text></div>
+        <div className="App">
+            <Frame timeslots={timeslots} frame_data={frame_data} />
+            <Nodes nodes={node_data} timeslots={timeslots} />
+        </div>
     );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,34 +1,135 @@
 // server/index.js
-const path = require('path');
 const express = require('express');
-const serport = require('serialport');
-const readline_parser = require('@serialport/parser-readline');
-const PORT = process.env.PORT || 3001;
 const app = express();
+const http = require('http');
+const Server = http.createServer(app);
+const io = require("socket.io")(Server, { cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] } });
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+//const index = require('./routes/index');
 
-const devport = new serport('/dev/ttyACM1');
-const rlparser = new readline_parser();
-devport.pipe(rlparser);
-var msg = "No data";
-rlparser.on('data', function (data) { msg = data; console.log(msg); });
+//app.use(index);
 
-// // Switches the port into "flowing mode"
-// devport.on('data', function (data) {
-//     console.log('Data:', data.to_string())
-//   })
+var gsocket = {};
+const Serial_Port = require('serialport');
+const Inter_Byte_Timeout = require('@serialport/parser-inter-byte-timeout')
+const devport = new Serial_Port('/dev/ttyACM1');
+const ibt_parser = devport.pipe(new Inter_Byte_Timeout({ interval: 50 }))
+function Node_Data() {
+    this.src_addr = 1;
+    this.dest_addr = 1;
+    this.data = 2;
+}
+function Timeslot_Packet() {
+    this.frame_ind = 1;
+    this.timeslot = 1;
+    this.timeslot_mask = 2;
+    this.total_node_count = 1;
+    this.removed_node_addr = 1;
+    this.future = 2;
+    this.node_data = new Node_Data();
+    this.fwd_data = [new Node_Data(), new Node_Data(), new Node_Data(), new Node_Data(), new Node_Data()];
+}
+
+function RTC_Source_Cycles() {
+    this.timeslot = 2;
+    this.listen = 1;
+    this.drift_listen = 1;
+    this.frame_extra_drift_listen = 1;
+    this.packet_listen = 1;
+    this.tx_to_rx_measured_delay = 1;
+}
+
+function RTC_Set_Values() {
+    this.rx_on = 2;
+    this.rx_packet_to_tx = 2;
+    this.rx_packet_to_rx = 2;
+    this.rx_no_packet_to_tx = 2;
+    this.rx_no_packet_to_rx = 2;
+    this.rx_no_packet_to_end_frame = 2;
+    this.tx_to_rx = 2;
+    this.tx_to_end_frame = 2;
+    this.timeslot = 2;
+}
+
+function Node_Control_Data() {
+    this.timeslots_per_frame = 1
+    this.startup_listen_frame_count = 1;
+    this.sleep_frame_count = 1;
+    this.total_node_count = 1;
+    this.our_timeslot = 1;
+    this.frame_ind = 1;
+    this.remove_next_frame = 1;
+    this.remove_this_frame = 1;
+    this.src_t = new RTC_Source_Cycles();
+    this.t = new RTC_Set_Values();
+}
+
+var tspacket_temp = new Timeslot_Packet();
+var frame_data = new Node_Control_Data();
+var node_data = [];
+var timeslots = {};
+
+var current_ind = 0;
+function recurse_obj_parse(obj, data) {
+    for (let [key, value] of Object.entries(obj)) {
+        if (typeof (value) === "object") {
+            recurse_obj_parse(value, data);
+        }
+        else {
+            let tmpv = value;
+            value = 0;
+            for (let i = 0; i < tmpv; ++i) {
+                value |= data[current_ind] << (i * 8);
+                ++current_ind;
+            }
+            obj[key] = value;
+        }
+    }
+}
+ibt_parser.on('data', function (data) {
+    if (data.length == 32) {
+        recurse_obj_parse(tspacket_temp, data);
+        timeslots[tspacket_temp.timeslot] = tspacket_temp;
+        tspacket_temp = new Timeslot_Packet();
+        current_ind = 0;
+    }
+    else if (data.length == 49) {
+        recurse_obj_parse(frame_data, data);
+        for (let i = 0; i < 8; ++i)
+        {
+            let val = data[current_ind + i*2];
+            val |= (data[current_ind + i*2 + 1] << 8);
+            node_data.push(val);
+        }
+        if (Object.keys(gsocket).length !== 0) {
+            gsocket.emit("FrameData", frame_data);
+            gsocket.emit("NodeData", node_data);
+            gsocket.emit("TimeslotData", timeslots);
+        }
+        frame_data = new Node_Control_Data();
+        node_data = [];
+        timeslots = {};
+        current_ind = 0;
+    }
+    else {
+        console.log("Invalid Data on Serial Port - Skipping");
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+    gsocket = socket;
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-    res.json({ message: msg });
-});
-
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+Server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
